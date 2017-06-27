f53080acefabb650be6815d4bbccd137f71afd7e
LeonardoManceroMafares <leonardo_mancero@hotmail.com>
2017-06-26 19:02:05 -0500

    original

---
+++ ejemplo1Git/G.php
@@ -0,0 +1,15 @@
+<html>
+<head></head>
+<body>
+
+<form action="ingresar.php" method="post">
+<input name="nombre" type="text"><br><br>
+<input name="cedula" type="text"><br><br>
+<input name="direccion" type="text"><br><br>
+<input name="telefono" type="text"><br><br>
+<input name="email" type="text"><br><br>
+<input type="submit" value="Enviar"><br><br>
+</form>
+
+</body>
+</html>
